5df380c5b2cb6f262bef63fe9dcd861909e6aa13
Guillaume Perrin <gperrin01@gmail.com>
2015-06-02 17:22:22 +0100

    basic structure, player model and players controllers, seeding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Player.delete_all
+
+p1 = Player.new(first_name: 'Michael', last_name: 'Jordan', teams: ['Bulls', 'Wizzards'], image: 'http://uk.eonline.com/eol_images/Entire_Site/2014513/rs_634x1141-140613051823-634.Michael-Jordan-JR-61314.jpg', video: 'https://www.youtube.com/watch?v=8gBSYqK4HkM' )
+
+p2 = Player.new(first_name: 'Tony', last_name: 'Parker', teams: ['Spurs'], image: 'http://www.axonpotential.com/wp-content/uploads/2013/05/Tony-Parker.jpg', video: 'www.youtube.com/watch?v=E7cYZ09t6kc' )
+
+p3 = Player.new(first_name: 'Stephen', last_name: 'Curry', teams: ['Warriors'], image: 'http://www.sikids.com/images/cms/imce/users/jrepanich/2013/04/stephen-curry-shooting-form.jpg' )
+
+
