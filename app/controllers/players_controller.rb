c3a8ee4d683153bc8789c31878ddf7a2882fa171
Guillaume Perrin <gperrin01@gmail.com>
2015-06-02 18:07:23 +0100

    index page showing all players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,40 @@
 class PlayersController < ApplicationController
+
+  def index
+    @players = Player.all
+  end
+
+  def new
+    # show the form
+  end
+
+  def create
+    # get params from the form
+    @player = Player.create(player_params)
+    redirect_to(players_path)
+  end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def edit
+    # show the form
+  end
+
+  def update
+    @player = Player.update(player_params)
+    redirect_to(player_path)
+  end
+
+  def destroy
+    @player = Player.find(params[:id])
+    @player.destroy
+    redirect_to(players_path)
+  end
+
+private
+  def player_params
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 Player.delete_all
 
-p1 = Player.new(first_name: 'Michael', last_name: 'Jordan', teams: ['Bulls', 'Wizzards'], image: 'http://uk.eonline.com/eol_images/Entire_Site/2014513/rs_634x1141-140613051823-634.Michael-Jordan-JR-61314.jpg', video: 'https://www.youtube.com/watch?v=8gBSYqK4HkM' )
+p1 = Player.create(first_name: 'Michael', last_name: 'Jordan', teams: ['Bulls', 'Wizzards'], image: 'http://uk.eonline.com/eol_images/Entire_Site/2014513/rs_634x1141-140613051823-634.Michael-Jordan-JR-61314.jpg', video: 'https://www.youtube.com/watch?v=8gBSYqK4HkM' )
 
-p2 = Player.new(first_name: 'Tony', last_name: 'Parker', teams: ['Spurs'], image: 'http://www.axonpotential.com/wp-content/uploads/2013/05/Tony-Parker.jpg', video: 'www.youtube.com/watch?v=E7cYZ09t6kc' )
+p2 = Player.create(first_name: 'Tony', last_name: 'Parker', teams: ['Spurs'], image: 'http://www.axonpotential.com/wp-content/uploads/2013/05/Tony-Parker.jpg', video: 'www.youtube.com/watch?v=E7cYZ09t6kc' )
 
-p3 = Player.new(first_name: 'Stephen', last_name: 'Curry', teams: ['Warriors'], image: 'http://www.sikids.com/images/cms/imce/users/jrepanich/2013/04/stephen-curry-shooting-form.jpg' )
+p3 = Player.create(first_name: 'Stephen', last_name: 'Curry', teams: ['Warriors'], image: 'http://www.sikids.com/images/cms/imce/users/jrepanich/2013/04/stephen-curry-shooting-form.jpg' )
 
 
